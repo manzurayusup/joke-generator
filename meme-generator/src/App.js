232126855c0c0fd1648dbb2c86966f6bc94fbeec
Manzura Yusupova <manzura@manzurasair133.attlocal.net>
2020-12-19 00:05:38 -0600

    created app

---
+++ meme-generator/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import NameAndGenderForm from './NameAndGenderForm';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+     <NameAndGenderForm />
+    );
+  }
+  
+}
+
+export default App;
