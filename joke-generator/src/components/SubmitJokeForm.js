987d6fa959b3933f9ba1f584c1a2d43d2aa5c36a
manzurayusup <manzurayusup@gmail.com>
2020-12-31 01:51:41 -0600

    submitJoke looks good

--- joke-generator/src/components/SubmitJokeForm.js
+++ joke-generator/src/components/SubmitJokeForm.js
@@ -5,14 +5,18 @@ import './SubmitJokeForm.css';
 function SubmitJokeForm(props) {
         return (
             <form id="jokeForm" method="POST">
+                
                 <strong>Submit a joke for approval:</strong>
+
                 <textarea name="jokeInput" placeholder="Your joke here..."></textarea>
+
                 <motion.input 
-                onClick={props.submitJoke}
-                whileHover={{ scale: 1.2, backgroundColor: '#ffff00' }}
-                whileTap={{ scale: 0.9 }}
-                type="submit" 
-                value="Submit" />
+                    onClick={props.submitJoke}
+                    whileHover={{ scale: 1.2, backgroundColor: '#ffff00' }}
+                    whileTap={{ scale: 0.9 }}
+                    type="submit" 
+                    value="Submit" />
+
             </form>
         );
 
