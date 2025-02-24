9171728a8d54d54dfd1bbbfdc27029b9f27ac62f
manzurayusup <manzurayusup@gmail.com>
2025-02-24 12:06:41 -0600

    Updates

---
+++ precache-manifest.85c2f6235e72490eda150e40fd09e0de.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e3d7b7b0f1f790ee8e768d348af9e5c",
+    "url": "/joke-generator/index.html"
+  },
+  {
+    "revision": "5277d618d22a17882bbe",
+    "url": "/joke-generator/static/css/main.6fd60866.chunk.css"
+  },
+  {
+    "revision": "00a069e863d9ee27d656",
+    "url": "/joke-generator/static/js/2.f2734ff9.chunk.js"
+  },
+  {
+    "revision": "043f8d872764d20e4400",
+    "url": "/joke-generator/static/js/3.659e45e1.chunk.js"
+  },
+  {
+    "revision": "5277d618d22a17882bbe",
+    "url": "/joke-generator/static/js/main.5871873a.chunk.js"
+  },
+  {
+    "revision": "3e471841466637c19d8e",
+    "url": "/joke-generator/static/js/runtime~main.f63ae89b.js"
+  },
+  {
+    "revision": "0d556915ebf9f3d31edf2f918f392d0d",
+    "url": "/joke-generator/static/media/emoji.0d556915.png"
+  },
+  {
+    "revision": "d2068f018b0d540b78031604dc198d36",
+    "url": "/joke-generator/static/media/me_and_peanut.d2068f01.jpg"
+  },
+  {
+    "revision": "ba9c4f7dec21f4bdaf0126a548831b05",
+    "url": "/joke-generator/static/media/yellow_mug.ba9c4f7d.png"
+  }
+]);
