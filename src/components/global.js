309dca77d21db84fa3b88324ee4fc9602cde581a
manzurayusup <manzurayusup@gmail.com>
2025-02-24 16:27:35 -0600

    removed global.js from tracking

--- src/components/global.js
+++
@@ -1,7 +0,0 @@
-/* contains variables used to connect to the API */
-
-const key = "REMOVED";
-const url = "https://joke3.p.rapidapi.com/v1/joke/%7Bid%7D";
-const host = "joke3.p.rapidapi.com";
-
-export {url, key, host};
